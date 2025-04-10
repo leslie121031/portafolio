e310672854510f0a94b3b68fd758f2529cb4fd46
leslie121031 <lesliearteta103@gmail.com>
2025-04-10 11:25:13 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+if (!Array.prototype.toSorted) {
+  Array.prototype.toSorted = function (compareFn) {
+    return this.slice().sort(compareFn);
+  };
+}
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
