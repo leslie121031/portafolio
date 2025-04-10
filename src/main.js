350f7744cea30a83f4b7d4e5b115ba15ec7e8f9a
leslie121031 <lesliearteta103@gmail.com>
2025-04-10 16:37:57 +0000

    codespaces

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-if (!Array.prototype.toSorted) {
-  Array.prototype.toSorted = function (compareFn) {
-    return this.slice().sort(compareFn);
-  };
-}
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
